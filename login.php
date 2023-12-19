8da303256ad1515598c6f1facdcd9bc6de60ccfa
Rama Audra <ramaaudra30.ra@gmail.com>
2023-12-20 00:48:36 +0700

    update tampilan dan class diagram

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 require 'database.php';
 include 'navbarlogin.php';
 
-if(!empty($_SESSION["id"])){
+if (!empty($_SESSION["id"])) {
   header("Location: index.php");
 }
 
@@ -11,35 +11,40 @@ if(!empty($_SESSION["id"])){
 
 <!doctype html>
 <ht lang="en">
+
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-IA-Compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>bayarBang.</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
-
+    <title>BayarBang</title>
+    <link rel="stylesheet" href="style/style.css">
   </head>
-  <body data-bs-theme="dark">
 
-<!-- form -->
+  <!-- form -->
 
-    
-      <form class="w-50 mx-auto p-5" action="aksilogin.php" method="post" autocomplete="off">
-        <div class="mb-3">
-          <label for="username" class="form-label">Username or Email</label>
-          <input type="text" class="form-control"  name="usernameemail">
-        </div>
-        <div class="mb-3">
-          <label class="form-label">Password</label>
-          <input type="password" class="form-control" name="password" required value="">
-        </div>
+  <body>
+    <div class="container">
+      <div class="box form-box">
+        <header>Login</header>
+        <form action="aksilogin.php" method="post" autocomplete="off">
+          <div class="field input">
+            <label for="username">Username</label>
+            <input type="text" name="usernameemail" id="usernameemail" required>
+          </div>
 
-        <button type="submit" name="submit" class="btn btn-outline-light" value="">Login</button>
-        <a href = "registration.php" class="btn btn-outline-light">Register</a>
-      </form>
-  
-  
-  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+          <div class="field input">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" required>
+          </div>
+
+          <div class="field">
+            <input type="submit" name="submit" value="login" required>
+          </div>
+
+          <div class="links">
+            Gak punya akun? <a href="registration.php">Daftar lah!</a>
+          </div>
+        </form>
+      </div>
+    </div>
   </body>
-</ht
